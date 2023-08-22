541465bbda77e775d906c69d3718e6e667be968b
shadenelnaggar <shadensherif987@gmail.com>
2023-08-22 12:31:35 -0700

    parameters.c done

--- main.h
+++ main.h
@@ -99,7 +99,7 @@ int print_number(char *str, params_t *params);
 int print_number_right_shift(char *str, params_t *params);
 int print_number_left_shift(char *str, params_t *params);
 
-/* params.c */
+/* parameters.c */
 void init_params(params_t *params, va_list va);
 
 /* precision.c */
